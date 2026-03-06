585ff0609f39e0be98f4d22f3f8a715ce3ef12a8
khanhdocnope <khanh4346k9@gmail.com>
2026-03-06 22:01:50 +0700

    add

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const API_BASE = "http://localhost:5000/api";
+const API_BASE = "https://rut-gon-link.onrender.com/api"; 
 
 // DOM Elements
 const shortenForm = document.getElementById("shortenForm");
